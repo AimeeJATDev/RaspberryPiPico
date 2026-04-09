3cef4579ebcaab831a9cb1adc88518fd2bfc4b28
AimeeJATDev <aimeejatdeveloper@outlook.com>
2026-04-09 21:05:43 +0100

    A couple more introductory programs from the RPi Pico book

---
+++ Pico2W/Reaction_Game.py
@@ -0,0 +1,30 @@
+import machine
+import time
+import random
+
+button_pressed = False
+right_btn = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
+led = machine.Pin(15, machine.Pin.OUT)
+left_btn = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+fastest_btn = None
+
+def btn_handler(pin):
+    global button_pressed
+    if not button_pressed:
+        button_pressed = True
+        global fastest_btn
+        fastest_btn = pin
+        
+led.value(1)
+time.sleep(random.uniform(5, 10))
+led.value(0)
+start_time = time.ticks_ms()
+left_btn.irq(trigger=machine.Pin.IRQ_FALLING, handler=btn_handler)
+right_btn.irq(trigger=machine.Pin.IRQ_FALLING, handler=btn_handler)
+
+while fastest_btn is None:
+    time.sleep(1)
+    if fastest_btn is left_btn:
+        print("Left Player Wins!")
+    elif fastest_btn is right_btn:
+        print("Right Player Wins!")
